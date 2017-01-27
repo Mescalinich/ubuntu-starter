8cc0bf83ecd1191884ebcb7a19f9f61108d3abe7
Alex Kalinkin <alex@kalinkin.info>
2017-01-27 17:48:42 +0300

    Update setup.sh
    NetworkManager openvpn applets installation

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# This script purposed to install some recently used packages
+# I have wrote it to speed up initial ubuntu tuning after clean installation
+
 echo "Ubuntu updated started"
 
 # ================================================================
@@ -18,3 +21,10 @@ sudo add-apt-repository ppa:webupd8team/java -y
 # ================================================================
 echo "Updating repository sources and packages"
 sudo apt-get update && sudo apt-get upgrade -y
+
+# ================================================================
+# Installing packages
+# ================================================================
+
+# NetworkManager applet (indicator) extensions to manage openvpn connections
+sudo apt-get install network-manager-openvpn network-manager-openvpn-gnome network-manager-pptp network-manager-vpnc -y
