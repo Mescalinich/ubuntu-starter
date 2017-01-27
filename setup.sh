40021f6e9b5fb05abc1f7d615fd39e2752c26d83
Alex Kalinkin <alex@kalinkin.info>
2017-01-27 17:39:06 +0300

    Update setup.sh
    Add repositories

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 echo "Ubuntu updated started"
 
+ecgo "Add repositories"
+# Add repositories
+# SimpleScreenRecorder to make screencasts
+sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder -y
+# flatpak - new linux package format
+sudo add-apt-repository ppa:alexlarsson/flatpak -y
+# Java
+sudo add-apt-repository ppa:webupd8team/java -y
+
 # Update repository sources and packages
 sudo apt-get update && sudo apt-get upgrade -y
