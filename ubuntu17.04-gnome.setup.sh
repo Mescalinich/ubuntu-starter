48e5ac2e8e86f68a05c509e6536bba6a5f93bc7c
Alex Kalinkin <alex@kalinkin.info>
2017-07-09 15:32:56 +0300

    Update ubuntu17.04-gnome.setup.sh

--- ubuntu17.04-gnome.setup.sh
+++ ubuntu17.04-gnome.setup.sh
@@ -1,1 +1,9 @@
-gg
+# Prepare system
+sudo apt update && sudo apt -y updrade
+
+# GnomeTweak tool
+sudo apt install -y gnome-tweak-tool 
+
+
+# Installing fonts
+sudo apt install -y fonts-font-awesome
